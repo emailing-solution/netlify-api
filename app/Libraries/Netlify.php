006cf7f2d66d65a789ec9dac78eb019008039bec
Mohamed Amine EL ATTABI <malohtie@gmail.com>
2021-09-24 18:59:53 +0100

    Fixs

--- app/Libraries/Netlify.php
+++ app/Libraries/Netlify.php
@@ -3,6 +3,7 @@
 namespace App\Libraries;
 
 use App\Models\Account;
+use Illuminate\Support\Facades\Http;
 
 class Netlify
 {
@@ -13,5 +14,50 @@ class Netlify
     public function __construct(Account $account)
     {
         $this->account = $account;
+    }
+
+    //get user information
+    public function user()
+    {
+        $request = Http::asJson()->withToken($this->account->token)
+            ->get(self::API_URL . "/user");
+        if($request->successful()) {
+            return $request->json();
+        }
+        return false;
+    }
+
+    //get accounts
+    public function accounts()
+    {
+        $request = Http::asJson()->withToken($this->account->token)
+            ->get(self::API_URL . "/accounts");
+        if($request->successful()) {
+            return $request->json();
+        }
+        return false;
+    }
+
+    public function sites(string $account)
+    {
+        $request = Http::asJson()->withToken($this->account->token)
+            ->get(sprintf("%s/%s/sites", self::API_URL, $account), [
+                'sort_by' => 'updated_at',
+                'per_page' => 100
+            ]);
+        if($request->successful()) {
+            return $request->json();
+        }
+        return false;
+    }
+
+    public function site(string $account, string $site)
+    {
+        $request = Http::asJson()->withToken($this->account->token)
+            ->get(sprintf("%s/%s/sites/%s", self::API_URL, $account, $site));
+        if($request->successful()) {
+            return $request->json();
+        }
+        return false;
     }
 }
